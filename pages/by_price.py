d24e713ca24aa54a8d965815aafa563cc00271d8
Daniel Plotnikov <daniilrew@gmail.com>
2024-04-06 20:51:18 +0500

    alpha-10 improve

--- pages/by_price.py
+++ pages/by_price.py
@@ -20,6 +20,25 @@ register_page(
     __name__, path="/by_price", icon="fa-solid:home", name="Цена | Логист. калькулятор"
 )
 
+allow_save_data_in_fields = False
+
+
+def get_tablerow_with_input(parameter_id=None, value_name=None, divider=False):
+    global allow_save_data_in_fields
+    return html.Tr(
+        [
+            html.Td(dcc.Markdown(value_name), style={"text-align": "left"}),
+            html.Td(
+                dbc.Input(
+                    placeholder=f"Введите {parameter_id}",
+                    id=f"price-{parameter_id}",
+                    type="number",
+                    persistence=allow_save_data_in_fields,
+                )
+            ),
+        ]
+    ) if divider==False else html.Tr(dmc.Divider())
+
 
 def layout():
     # return html.A("Раздел не готов. Перейдите к расчетам по времени.", href="/by_time")
@@ -29,10 +48,35 @@ def layout():
             html.Div(
                 [
                     # оформить ввод данных в виде таблицы
-                    dbc.InputGroup([dbc.InputGroupText('Description'), dbc.Input(type='number')]),
+                    html.Table(
+                        html.Tbody(
+                            [
+                                # get_tablerow_with_input("R1", "Test"),
+                                # html.Tr(dcc.Markdown('**header**')),
+                                html.Tr(dcc.Markdown('**Основные расходы**')),
+                                get_tablerow_with_input("R1", "Расходы на аренду 1 кв.м площади"),
+                                get_tablerow_with_input("R2", "Коммунальные платежи на 1 кв.м"),
+                                get_tablerow_with_input("R3", "Прочие расходы на экспл. и содержание склада"),
+                                html.Tr(dcc.Markdown('**Амортизация**')),
+                                get_tablerow_with_input("A1", "Срок амортизации стеллажей в мес."),
+                                get_tablerow_with_input("A2", "Срок амортизации прочего скл. оборудования в мес."),
+                                html.Tr(dcc.Markdown('**Расходы на эксплуатацию**')),
+                                get_tablerow_with_input("C1", "Стоимость стеллажей"),
+                                get_tablerow_with_input("C2", "Обслуживание стеллажей"),
+                                get_tablerow_with_input("C3", "Стоимость прочего скл. оборудования"),
+                                get_tablerow_with_input("C4", "Прочие расходы на эксплуатацию"),
+                            ]
+                        )
+                    ),
+                    html.Table(
+                        html.Tbody(
+                            [
+                                
+                            ]
+                        )
+                    ),
                 ],
                 className="input-block",
-                style={"height": "200px"},
             ),
         ],
         align="center",
